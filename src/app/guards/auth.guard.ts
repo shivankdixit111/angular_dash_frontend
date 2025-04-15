209c5e922e55e6b692faa2be8e4180af796f3e5f
Shivank Dixit <shivankdixit@Shivanks-MacBook-Air.local>
2025-04-15 20:58:05 +0530

    pushed

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,15 +3,12 @@ import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core'; 
 
 export const authGuard: CanActivateFn = (route, state) => {
-
- 
    
   const authService = inject(AuthService);
   const router = inject(Router); 
 
   const isLoggedIn = authService.isLoggedInSubject.getValue();
-
-  console.log('auth guard is ', isLoggedIn)
+ 
   if(isLoggedIn) {
     return true;
   } else {
